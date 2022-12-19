0c5776f8897c70f3678689ee9a32d2d305a0f149
Allen <t_araki@yumemi.co.jp>
2022-12-20 07:53:57 +0900

    fix: 線を残すようにしてみた

--- src/sketchs/example.ts
+++ src/sketchs/example.ts
@@ -6,16 +6,16 @@ const exampleSketch = (p5: p5) => {
     HEIGHT: 800,
   };
 
-  const points = new Array(1200).fill(0).map(() => {
+  const points = new Array(2000).fill(0).map(() => {
     return {
       position: p5.createVector(
         Math.random() * CANVAS_META.WIDTH,
         Math.random() * CANVAS_META.HEIGHT,
       ),
       color: [
-        100 * Math.random(), // 色相
-        100 * (1 - Math.random() * Math.random()), // 彩度
-        50 // 輝度
+        20 * Math.random() + 10, // 色相
+        40 * (1 - Math.random() * Math.random()) + 30, // 彩度
+        30 + Math.random() * 20,  // 輝度
       ],
     };
   });
@@ -27,15 +27,20 @@ const exampleSketch = (p5: p5) => {
     p5.colorMode(p5.HSL, 100);
     p5.stroke(100, 100, 100, 0);
     p5.noiseSeed(Math.random() * 1000);
+    p5.background(0, 0, 0, 255);
+    p5.background(
+        20 * Math.random() + 50, // 色相
+        40 * (1 - Math.random() * Math.random()) + 10, // 彩度
+        50// 輝度
+    );
   };
 
   p5.draw = () => {
-    p5.background(0, 0, 0, 0);
+    // p5.background(0, 0, 0, 0);
     points.forEach((val, index) => {
-
       val.position.set((val.position.x + CANVAS_META.WIDTH) % CANVAS_META.WIDTH, (CANVAS_META.HEIGHT + val.position.y) % CANVAS_META.HEIGHT)
 
-      const group = index % 3;
+      const group = index % 10;
       val.position.add(Math.sin(val.position.y / 40 + group / 3) - 0.2, -2);
 
       val.position.add(
@@ -44,7 +49,7 @@ const exampleSketch = (p5: p5) => {
       );
 
       p5.fill(val.color);
-      p5.ellipse(val.position.x, val.position.y, 4, 4);
+      p5.square(val.position.x, val.position.y, 5);
 
     });
   };
