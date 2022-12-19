452b4b86129a05616611ca96e230a9c1445cccdd
Allen <t_araki@yumemi.co.jp>
2022-12-19 21:08:43 +0900

    Revert "fix: pathの状態が保存できていないためページが表示されない問題を訂正"
    This reverts commit de84aed8dc166e4bf71854fe4d2f258d68aa8f51.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  build: {
-    target: "esnext",
-    lib: {
-      fileName: "mod",
-      formats: ["es"],
-      entry: path.join(__dirname, "src/mod.ts"),
-    },
-  },
 })
